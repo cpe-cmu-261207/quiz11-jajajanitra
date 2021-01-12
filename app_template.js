e21f901897d9ebaf406dbfd00079c32f67fa4aed
janitra <jajajanitra@gmail.com>
2021-01-12 13:56:54 +0700

    up

--- app_template.js
+++ app_template.js
@@ -1,1 +1,14 @@
 //your code here!
+let number = document.getElementById("input-number")
+let modifyButton = document.getElementById("button-compute")
+let myID = 620610781
+let result = document.getElementById("compute-result")
+let x
+
+modifyButton.addEventListener("click",() => {
+    x = myID-number
+    result.innerHTML(x)
+})
+
+let tr = document.createElement('tr')
+let td = document.createElement('td')
